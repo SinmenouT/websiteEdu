a45b8003fef6bcee0d401eb4f28668133647653a
SinmenouT <163652839+SinmenouT@users.noreply.github.com>
2024-12-23 21:53:03 +0000

    Update admin.php

--- admin.php
+++ admin.php
@@ -2,7 +2,7 @@
 <?php
 // Informations de connexion à la base de données
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'concours');
+define('DB_NAME', 'con');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
